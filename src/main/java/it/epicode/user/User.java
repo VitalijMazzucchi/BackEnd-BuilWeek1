e3a8b9b36c37cbc46279659192da8704c3ebb68d
albertiniclaudia <claudia.albertini@live.it>
2022-08-08 15:56:00 +0200

    class user

--- src/main/java/it/epicode/user/User.java
+++ src/main/java/it/epicode/user/User.java
@@ -5,4 +5,17 @@ public class User {
 	private String surname;
 	
 	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
 }
