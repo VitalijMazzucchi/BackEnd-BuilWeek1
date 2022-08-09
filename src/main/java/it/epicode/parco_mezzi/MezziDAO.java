58d8a8d9fd85b6709d321fba4d350ea5ce50ec84
Leo Folle <folleleo29@gmail.com>
2022-08-09 13:01:52 +0200

    commit modifiche generali pausa pranzo prendete un coffeeeee

--- src/main/java/it/epicode/parco_mezzi/MezziDAO.java
+++ src/main/java/it/epicode/parco_mezzi/MezziDAO.java
@@ -2,6 +2,7 @@ package it.epicode.parco_mezzi;
 
 import javax.persistence.EntityManager;
 
+import it.epicode.biglietti.Biglietto;
 import jpautil.JpaUtil;
 
 public class MezziDAO {
@@ -31,6 +32,15 @@ public class MezziDAO {
 		EntityManager em = JpaUtil.getEntityManagerFactory().createEntityManager();
 		em.getTransaction().begin();
 		em.merge(mezzo);
+		em.getTransaction().commit();
+		em.close();
+	}
+	public void timbra(Integer numBiglietto) {
+		EntityManager em = JpaUtil.getEntityManagerFactory().createEntityManager();
+		em.getTransaction().begin();
+		Biglietto biglietto = em.find(Biglietto.class, numBiglietto);
+		biglietto.setValido(false);
+		em.merge(biglietto);
 		em.getTransaction().commit();
 		em.close();
 	}
